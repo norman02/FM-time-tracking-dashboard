27357d8361727ef3382a1d55eacadbd56111c5a5
John N. Norman <normanj777@gmail.com>
2022-01-20 08:27:12 -0500

    refactor createTrackerCard() to accept params

--- js/app.js
+++ js/app.js
@@ -1,49 +1,64 @@
-const tracker = document.getElementById('js-tracker')
+const tracker = document.getElementById("js-tracker");
 
 /**
  * create a tracker card
+ * @param image (string) URL of the background image
+ * @param name (string) the name of the activity
+ * @param hours (number)number of hours completed this session
+ * @param lastHours (number) of hours completed last session
  */
-const createTrackerCard = () => {
-    /** create tracker card */
-    const trackerCard = document.createElement('section')
-    trackerCard.classList.add('tracker__card')
-    tracker.appendChild(trackerCard)
-    /** create background element */
-    const trackerCardBackground = document.createElement('div')
-    trackerCardBackground.classList.add('tracker__card--bg')
-    trackerCard.appendChild(trackerCardBackground)
-    const backgroundImage = document.createElement('img') 
-    backgroundImage.src = './images/icon-work.svg' 
-    trackerCardBackground.appendChild(backgroundImage)
-    /** create data element */
-    const trackerCardData = document.createElement('div')
-    trackerCardData.classList.add('tracker__card--data')
-    trackerCard.appendChild(trackerCardData)
-    /** Add content to data element */
-    // create title
-    const activityName = document.createElement('h2')
-    activityName.textContent = 'Work'
-    trackerCardData.appendChild(activityName)
-    // create menue link
-    const ellipsis = document.createElement('img')
-    ellipsis.src = './images/icon-ellipsis.svg'
-    const link = document.createElement('a')
-    link.appendChild(ellipsis)
-    trackerCardData.appendChild(link)
-    // create current hours element
-    const currentHours = document.createElement('p')
-    currentHours.classList.add('xl-body')
-    currentHours.textContent = '0hrs'
-    trackerCardData.appendChild(currentHours)
-    // creat previous week hours
-    const prevHours = document.createElement('p')
-    prevHours.textContent = `Last week - 0hrs`
-    prevHours.classList.add('light-text')
-    prevHours.classList.add('justify-end')
-    trackerCardData.appendChild(prevHours)
+const createTrackerCard = (image = "", name = 'Activity', hours = 0, lastHours = 0) => {
+  /** create tracker card */
+  const trackerCard = document.createElement("section");
+  trackerCard.classList.add("tracker__card");
+  tracker.appendChild(trackerCard);
+  /** create background element 
+   * @param imageUrl (string) URL for the background image
+  */
+  const createTrackerCardBackground = (imageUrl) => {
+    const trackerCardBackground = document.createElement("div");
+    trackerCardBackground.classList.add("tracker__card--bg");
+    trackerCard.appendChild(trackerCardBackground);
+    const backgroundImage = document.createElement("img");
+    backgroundImage.src = imageUrl;
+    trackerCardBackground.appendChild(backgroundImage);
+  }
+  createTrackerCardBackground(image)
   
-}
-createTrackerCard()
-createTrackerCard()
-createTrackerCard()
-createTrackerCard()
+  /** create data element */
+  const trackerCardData = document.createElement("div");
+  trackerCardData.classList.add("tracker__card--data");
+  trackerCard.appendChild(trackerCardData);
+  /** Add content to data element
+   * @param activityName the name of the activity
+   * @param currentHours hours completed this session
+   * @param prevHours hours completed last session
+   */
+  const addDataCardContent = (activityName, currentHours, prevHours) => {
+    // create title
+    const activityNameField = document.createElement("h2");
+    activityNameField.textContent = activityName;
+    trackerCardData.appendChild(activityNameField);
+    // create menue link
+    const ellipsis = document.createElement("img");
+    ellipsis.src = "./images/icon-ellipsis.svg";
+    const link = document.createElement("a");
+    link.appendChild(ellipsis);
+    trackerCardData.appendChild(link);
+    // create current hours element
+    const currentHoursField = document.createElement("p");
+    currentHoursField.classList.add("xl-body");
+    currentHoursField.textContent = currentHours + "hrs";
+    trackerCardData.appendChild(currentHoursField);
+    // creat previous week hours
+    const prevHoursField = document.createElement("p");
+    prevHoursField.textContent = `Last week - ${prevHours}hrs`;
+    prevHoursField.classList.add("light-text");
+    prevHoursField.classList.add("justify-end");
+    trackerCardData.appendChild(prevHoursField);
+  };
+
+
+  addDataCardContent(name, hours, lastHours);
+};
+createTrackerCard("./images/icon-work.svg", 'work', 30, 40);
