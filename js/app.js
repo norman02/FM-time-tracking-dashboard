c335725255366c893d334f14dcd24ad3ea0767b6
John N. Norman <normanj777@gmail.com>
2022-01-20 07:35:16 -0500

    add function to create a tracking card

--- js/app.js
+++ js/app.js
@@ -1,44 +1,47 @@
 const tracker = document.getElementById('js-tracker')
-/**
- * add hellowWorld to the tracker secection
- */
-const helloWorld = () => {
-    
-    const myHeader = document.createElement('h1')
-    myHeader.textContent = 'hello world'
-    tracker.appendChild(myHeader)
-    console.log('Hello')
-
-    
-}
-
-
-/**
- * create a background element
- *  1. create div class tracker__card--bg
- *  2. create img
- */
-const createBackground = () => {
-    const background = tracker.createElement('div')
-    background.addClass('tracker__card--bg')
-    /** TODO: add image */
-}
-
-/**
- * create a data element
- */
 
 /**
  * create a tracker card
- *  1. create section
- *  1. create background elment
- *  2. create data element
- *  3. append background element
- *  4. append data element
  */
 const createTrackerCard = () => {
-    helloWorld()
-
+    /** create tracker card */
+    const trackerCard = document.createElement('section')
+    trackerCard.classList.add('tracker__card')
+    tracker.appendChild(trackerCard)
+    /** create background element */
+    const trackerCardBackground = document.createElement('div')
+    trackerCardBackground.classList.add('tracker__card--bg')
+    trackerCard.appendChild(trackerCardBackground)
+    const backgroundImage = document.createElement('img') 
+    backgroundImage.src = './images/icon-work.svg' 
+    trackerCardBackground.appendChild(backgroundImage)
+    /** create data element */
+    const trackerCardData = document.createElement('div')
+    trackerCardData.classList.add('tracker__card--data')
+    trackerCard.appendChild(trackerCardData)
+    /** Add content to data element */
+    // create title
+    const activityName = document.createElement('h2')
+    activityName.textContent = 'Work'
+    trackerCardData.appendChild(activityName)
+    // create menue link
+    const ellipsis = document.createElement('img')
+    ellipsis.src = './images/icon-ellipsis.svg'
+    const link = document.createElement('a')
+    link.appendChild(ellipsis)
+    trackerCardData.appendChild(link)
+    // create current hours element
+    const currentHours = document.createElement('p')
+    currentHours.classList.add('xl-body')
+    currentHours.textContent = '0hrs'
+    trackerCardData.appendChild(currentHours)
+    // creat previous week hours
+    const prevHours = document.createElement('p')
+    prevHours.textContent = `Last week - 0hrs`
+    prevHours.classList.add('light-text')
+    prevHours.classList.add('justify-end')
+    trackerCardData.appendChild(prevHours)
+  
 }
 
 createTrackerCard()
